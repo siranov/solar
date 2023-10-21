cfd6b504c08e1b02c948c83734a22501aa94b08f
Zhangir <siranoov@gmail.com>
2023-10-20 19:23:37 -0700

    wtf

--- pages/raffle/index.js
+++ pages/raffle/index.js
@@ -1,6 +1,6 @@
 import 'app/global.css'
 import co from 'counter.json'
-import { React, useState } from './react'
+import { React, useState } from 'react'
 // import axios from 'node_modules/axios';
 
 
